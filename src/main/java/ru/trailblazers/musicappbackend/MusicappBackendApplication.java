151405a7db6b976b53820296de60be39b14b11d7
yudiplease <dregoinger@gmail.com>
2023-11-25 15:00:34 +0300

    init

---
+++ src/main/java/ru/trailblazers/musicappbackend/MusicappBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.trailblazers.musicappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicappBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicappBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/trailblazers/musicappbackend/MusicappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.trailblazers.musicappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicappBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
